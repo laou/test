85df183478246425d5cc8fbe35d0a5f6558cbde8
Guillaume Lucas <guillaume.lucas@imina.ch>
2014-11-20 11:22:05 +0100

    func added
    Signed-off-by: Guillaume Lucas <guillaume.lucas@imina.ch>

---
+++ project2/include/func.h
@@ -0,0 +1,29 @@
+/*
+////////////////////////////////////////////////////////////////////////////////
+//
+//  This file is part of IMINA SOFTWARE CONTROL LIBRARY.
+//
+//  (C) 2008 - 2014 Imina Technologies, Lausanne, Switzerland.
+//  All Rights Reserved.
+//
+//  CONFIDENTIAL
+//  Do not redistribute without express consent from copyright holder.
+//  Contact: benoit.dagon@imina.ch
+//
+//  Filename: func.h
+//  Created:  20.11.2014
+//  Author:   guillaume
+//
+////////////////////////////////////////////////////////////////////////////////
+*/
+
+#ifndef FUNC_H
+#define FUNC_H
+
+namespace project2 {
+
+void doSomething();
+
+}
+
+#endif // FUNC_H
---
+++ project2/src/func.cpp
@@ -0,0 +1,33 @@
+/*
+////////////////////////////////////////////////////////////////////////////////
+//
+//  This file is part of IMINA SOFTWARE CONTROL LIBRARY.
+//
+//  (C) 2008 - 2014 Imina Technologies, Lausanne, Switzerland.
+//  All Rights Reserved.
+//
+//  CONFIDENTIAL
+//  Do not redistribute without express consent from copyright holder.
+//  Contact: benoit.dagon@imina.ch
+//
+//  Filename: func.cpp
+//  Created:  20.11.2014
+//  Author:   guillaume
+//
+////////////////////////////////////////////////////////////////////////////////
+*/
+
+
+#include "func.h"
+
+#include <iostream>
+using namespace std;
+
+namespace project2 {
+
+void doSomething()
+{
+    cout << "do something!" << endl;
+}
+
+}
